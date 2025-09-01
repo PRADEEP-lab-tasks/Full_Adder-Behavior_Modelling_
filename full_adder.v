971045783ad03c8c989b810d42f8d68165d70d1c
PRADEEP-lab-tasks <loganathanp730@gmail.com>
2025-09-02 00:29:25 +0530

    Add files via upload

---
+++ full_adder.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+
+
+module full_adder (
+    input wire a, b, cin,
+    output wire sum, cout
+);
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (cin & (a ^ b));
+endmodule
---
+++ testbench.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/07/2024 04:51:21 PM
+// Design Name: 
+// Module Name: testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench;
+    reg a, b, cin;
+    wire sum, carry;
+
+    full_adder uut(a, b, cin, sum, carry);
+
+    initial begin
+        // Test cases
+        a = 0; b = 0; cin = 0; #10;
+        a = 0; b = 0; cin = 1; #10;
+        a = 0; b = 1; cin = 0; #10;
+        a = 0; b = 1; cin = 1; #10;
+        a = 1; b = 0; cin = 0; #10;
+        a = 1; b = 0; cin = 1; #10;
+        a = 1; b = 1; cin = 0; #10;
+        a = 1; b = 1; cin = 1; #10;
+
+        $finish();
+    end
+endmodule
+
+
+
+
